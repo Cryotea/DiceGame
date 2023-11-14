8e6c1546ab9d70d0d324adf4480b85bb19e88826
Cryotea <cryotea@gmail.com>
2023-11-14 17:51:50 +0100

    added ShowStats updatet OpenInventory

--- Player.cs
+++ Player.cs
@@ -15,21 +15,33 @@ public class Player : IFighter
 
     public (int, IItem) [] ItemList =
     {
-       (1, new SmallPotion())
+       (1, new SmallPotion()),
+       (1, new MediumPotion())
     };
 
+/* 
+wurde hier gerne die inventory numbers zum auswahlen nicht hard coden
+sodass items nicht angezeigt werden wenn man 0 von ihnen hat, meine
+idee ware die in einen temporare arry zu tun wenn sie mehr als 0 items
+davon habe jedoch weiss ich nicht wie oder ob das geht dabei brauche 
+ich deine hilfe
+*/
     public void OpenInventory()
     {
-        Console.WriteLine(ItemList);
-        Console.WriteLine("press enter to go back");
-        Console.ReadLine();
+        foreach (var Items in ItemList)
+        {
+            Console.WriteLine($"|{Items.Item2} x {Items.Item1} |");
+        }
+        Console.WriteLine("press X to go back");
+        string input = Console.ReadLine();
+        
     }
 
     public bool PlayerMove(Player player, IFighter enemy, bool usedMove)
     {
         
         Console.WriteLine($"what is {player.Name} doing ");
-        Console.WriteLine("1 = Attack 2 = Inventory");
+        Console.WriteLine($"|1 = Attack |2 = Inventory |3 = {player.Name}'s Stats |");
         string input = Console.ReadLine();
         if (input == "1")
         {
@@ -43,9 +55,21 @@ public class Player : IFighter
             usedMove = false;
             return usedMove;
         }
+        if (input == "3")
+        {
+            player.ShowStats();
+        }
         else Console.WriteLine("Please Input a Valid Number");
         usedMove = false;
         return usedMove;
         
     }
+
+    public void ShowStats()
+    {
+        Console.WriteLine($"Name :{Name} \tHP:{Health}/{MaxHealth} \nDMG:{Damage} \tWeapon:{Weapon} \nSpeed:{Speed} \tDefence:{Defence} ");
+        Console.WriteLine("press enter to go back");
+        Console.ReadLine();
+    }
+
 }
