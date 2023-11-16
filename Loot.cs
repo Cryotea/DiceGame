563fca4b9353b0a0d86e005967f5c09e5bef9917
Cryotea <cryotea@gmail.com>
2023-11-16 18:13:06 +0100

    added Loot Mechanik

--- Loot.cs
+++ Loot.cs
@@ -1,45 +1,89 @@
 
 using diceGame.Item;
 using diceGame;
+using diceGame.Weapon;
 namespace diceGame;
 
 
 public class Loot
 {
-    IItem[] lootPool = 
-    {
-        new SmallPotion(),
-        new MediumPotion(),
-    };
-
-    IItem[] premiumLootPool =
-    {
-        new GoldenFonduePizza(),
-    };
 
     public void GetLoot(Player player, IFighter enemy)
     {
         var random = new Random();
-        int WeaponDrop = random.Next(1,4);
-        int SpecialDrop = random.Next(1,4);
-        if (WeaponDrop == 1)
-        {
-            int WeaponIndex = Array.IndexOf(player.WeaponList, enemy.Weapon);
-            player.WeaponList[WeaponIndex].Item1 = true;
-        }
-
-        IItem loot = lootPool[random.Next(0, lootPool.Length)];
-        int itemNumber = Array.IndexOf(player.ItemList,loot);
-        player.ItemList[itemNumber].Item1++;
-        Console.WriteLine($"{player.Name} got a {loot}!");
+        int Loot = random.Next(1,3);
+        int RareLootChance = random.Next(1,5);
+        int WeaponDropChance = random.Next(1,4);
         
-        if (SpecialDrop == 1)
+
+        if(Loot == 1)
         {
-            IItem specialloot = premiumLootPool[random.Next(0, lootPool.Length)];
-            int specialitemNumber = Array.IndexOf(player.ItemList, specialloot);
-            player.ItemList[specialitemNumber].Item1++;
-            Console.WriteLine($"{player.Name} got a {specialloot}!");
+            player.Inventory.SmallPotion.Item1 ++;
+            Console.WriteLine($"{player.Name} got a {player.Inventory.SmallPotion.Item2}");
+        }
+        if(Loot == 2)
+        {
+            player.Inventory.MediumPotion.Item1++;
+            Console.WriteLine($"{player.Name} got a {player.Inventory.MediumPotion.Item2}");
         }
 
+        if (RareLootChance == 1)
+        {
+            int RareLoot = random.Next(1,2);
+
+            if (RareLoot == 1)
+            {
+                player.Inventory.GoldenFonduePizza.Item1++;
+                Console.WriteLine($"{player.Name} got a {player.Inventory.GoldenFonduePizza.Item2}");
+            }
+        }
+
+        if (WeaponDropChance == 1)
+        {
+            enemy.Weapon.Loot(player);
+        }
     }
+/*        public void GetLoot(IFighter enemy, Player player)
+    {
+        (int, IItem)[] LootPool;
+        (int, IItem)[] RareLootPool;
+        (int, IWeapon)[] PlayerWeapons;
+        LootPool = new (int , IItem)[]
+        {
+            player.Inventory.SmallPotion,
+            player.Inventory.MediumPotion,
+        };
+        RareLootPool = new (int, IItem)[]
+        {
+            player.Inventory.GoldenFonduePizza,
+        };
+        PlayerWeapons = new (int, IWeapon)[]
+        {
+            player.Inventory.NoWeapon,
+            player.Inventory.CopperShortSword,
+            player.Inventory.IronSword,
+            player.Inventory.Bow,
+        };
+
+       var random = new Random();
+       int RareDrop = random.Next(1,5);
+       int WeaponDrop = random.Next (1,5);
+
+       if (RareDrop == 1)
+       {
+        int RareLoot = random.Next(0, RareLootPool.Length);
+        RareLootPool[RareLoot].Item1++;
+        Console.WriteLine($"{player.Name} got {RareLootPool[RareLoot].Item2}");
+       }
+
+       if(WeaponDrop == 1)
+       {
+
+       }
+
+       int loot = random.Next(0, LootPool.Length) ;
+       LootPool[loot].Item1++;
+       Console.WriteLine($"{player.Name} got { LootPool[loot].Item2}");
+    }
+    */
 }
