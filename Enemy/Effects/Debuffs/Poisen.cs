bd3950a39ccbcfd01cf0b1aa6a5afc213164f111
Cryotea <cryotea@gmail.com>
2023-11-18 18:02:03 +0100

    Added Poisen Class and Debuff Methode

---
+++ Enemy/Effects/Debuffs/Poisen.cs
@@ -0,0 +1,11 @@
+namespace diceGame.Effects.Debuffs;
+
+public class Poisen : IDebuff
+{
+    public static void Debuff(IFighter user , int Level, int duration)
+    {
+        double Poisen = user.Health * ( Level * 0.1);
+        user.Health = user.Health - (user.Health * ( Level * 0.1));
+        Console.WriteLine($"{user.Name} took Poisen Damage for {Poisen}! ");
+    }
+}
