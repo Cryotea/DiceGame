45acf8ff7d7816e6646d3af61fe366c477187bc3
Cryotea <cryotea@gmail.com>
2023-11-20 14:01:29 +0100

    added MaxHealth cap

--- Effects/Buffs/Regenaration.cs
+++ Effects/Buffs/Regenaration.cs
@@ -4,8 +4,12 @@ public class Regenaration : IBuff
 {
     public static void Buff(IFighter user, int Level, int duration)
     {
-        double Healed = user.Health * ( Level * 0.1);
-        user.Health = user.Health + (user.Health * ( Level * 0.1));
-        Console.WriteLine($"{user.Name} got healed for {Healed}! ");
+        double Healed = user.MaxHealth * ( Level * 0.1);
+        user.Health = user.Health + (user.MaxHealth * ( Level * 0.1));
+        if (user.Health > user.MaxHealth)
+        {
+            user.Health = user.MaxHealth;
+        }
+        Console.WriteLine($"{user.Name}'s Regenaration healed them for {Healed}!\n{user.Name} has {user.Health}Hp now! \nThe duration is of Regenaration is {user.Effect.Regenaration.Item2}");
     }
 }
