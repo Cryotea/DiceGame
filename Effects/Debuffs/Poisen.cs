d3e76e355ec7d41f9cbf70dd9aa96790f7ff0940
Cryotea <cryotea@gmail.com>
2023-11-20 22:09:43 +0100

    fixed Poisen Damage calculation

--- Effects/Debuffs/Poisen.cs
+++ Effects/Debuffs/Poisen.cs
@@ -4,8 +4,8 @@ public class Poisen : IDebuff
 {
     public static void Debuff(IFighter user , int Level, int duration)
     {
-        double Poisen = user.Health * ( Level * 0.1);
-        user.Health = user.Health - (user.Health * ( Level * 0.1));
+        double Poisen = user.MaxHealth * ( Level * 0.1);
+        user.Health = user.Health - (user.MaxHealth * ( Level * 0.1));
         Console.WriteLine($"{user.Name} took Poisen Damage for {Poisen}!\nThe duration is {user.Effect.Poisen.Item2} ");
     }
 }
