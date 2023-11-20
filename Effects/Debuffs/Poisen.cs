d1655b965ba955af19220628ed4d3d9341dedc75
Cryotea <cryotea@gmail.com>
2023-11-20 14:04:34 +0100

    added a display for the duration of poisen

--- Effects/Debuffs/Poisen.cs
+++ Effects/Debuffs/Poisen.cs
@@ -6,6 +6,6 @@ public class Poisen : IDebuff
     {
         double Poisen = user.Health * ( Level * 0.1);
         user.Health = user.Health - (user.Health * ( Level * 0.1));
-        Console.WriteLine($"{user.Name} took Poisen Damage for {Poisen}! ");
+        Console.WriteLine($"{user.Name} took Poisen Damage for {Poisen}!\nThe duration is {user.Effect.Poisen.Item2} ");
     }
 }
