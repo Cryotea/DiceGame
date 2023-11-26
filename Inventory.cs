c55467a3d5fb1f8e4df5b23f9834c51f2ae5c7aa
Cryotea <cryotea@gmail.com>
2023-11-26 14:42:49 +0100

    edditet Inventory to use new Amount Property

--- Inventory.cs
+++ Inventory.cs
@@ -5,47 +5,59 @@ namespace diceGame;
 
 public class Inventory
 {
+
+    public IItem[] AllItems 
+    {
+        get 
+        {
+            return new IItem[]
+            {
+                SmallPotion,
+                MediumPotion,
+                GoldenFonduePizza,
+                PotionOfRegenaration
+            };
+        }
+    }
+
+    public IWeapon[] AllWeapons
+    {
+        get
+        {
+            return new IWeapon[]
+            {
+                NoWeapon,
+                CopperShortSword,
+                IronSword,
+                Bow,
+                PoisenCrossbow,
+                Shovel
+            };
+        }
+    }
     // normal Items
-    public (int, IItem) SmallPotion = (1, new SmallPotion());
-    public (int, IItem) MediumPotion = (1, new MediumPotion());
-    public (int, IItem) PotionOfRegenaration = (1, new PotionOfRegenaration());
+    public IItem SmallPotion = new SmallPotion();
+    public IItem MediumPotion = new MediumPotion();
+    public IItem PotionOfRegenaration = new PotionOfRegenaration();
 
     // rare Items
-    public (int, IItem) GoldenFonduePizza = (0, new GoldenFonduePizza());
+    public IItem GoldenFonduePizza = new GoldenFonduePizza();
 
     // Weapons
-    public (int, IWeapon) NoWeapon = (1, new NoWeapon());
-    public (int, IWeapon) CopperShortSword = (1, new CopperShortSword());
-    public (int, IWeapon) IronSword = (0, new IronSword());
-    public (int, IWeapon) Bow = (0, new Bow()); 
-    public (int, IWeapon) PoisenCrossbow = (0, new PoisenCrossbow());
+    public IWeapon NoWeapon = new NoWeapon();
+    public IWeapon CopperShortSword = new CopperShortSword();
+    public IWeapon IronSword = new IronSword();
+    public IWeapon Bow = new Bow(); 
+    public IWeapon PoisenCrossbow = new PoisenCrossbow();
+    public IWeapon Shovel = new Shovel();
 
     public void OpenInventory(Player player)
     {
-        (int, IItem)[] AllItems;
-        (int, IWeapon)[] AllWeapons;
-        
-        AllItems = new (int, IItem)[]
-        {
-            SmallPotion,
-            MediumPotion,
-            GoldenFonduePizza,
-            PotionOfRegenaration
-        };
 
-         AllWeapons = new (int, IWeapon)[]
-        {
-            NoWeapon,
-            CopperShortSword,
-            IronSword,
-            Bow,
-            PoisenCrossbow
-        };
-
-        var notEmptyItems = AllItems.Where(item => item.Item1 > 0).ToList();
+        var notEmptyItems = AllItems.Where(item => item.Amount > 0).ToList();
         foreach (var Items in notEmptyItems)
         {
-            Console.WriteLine($"|{Items.Item2} x {Items.Item1} |");
+            Console.WriteLine($"|{Items} x {Items.Amount} |");
         }
         Console.WriteLine("press X to go back\n press E to equip a different Weapon");
         string input = Console.ReadLine();
@@ -54,19 +66,19 @@ public class Inventory
 
         if (input != null && input.ToLower() == "e")
         {
-            var ownedWeapons = AllWeapons.Where(Weapon => Weapon.Item1 >0).ToList();
+            var ownedWeapons = AllWeapons.Where(Weapon => Weapon.Amount >0).ToList();
             foreach (var Weapon in ownedWeapons)
             {
-                Console.WriteLine($"|{Weapon.Item2} |");
+                Console.WriteLine($"|{Weapon} |");
             }
             Console.WriteLine("Input the Nuber from the Weapon to equip it\n or press x to go back");
             string weaponinput = Console.ReadLine();
 
             if (input != null && input.ToLower() == "x") OpenInventory(player);
 
-             if (int.TryParse(weaponinput, out int parsedInput2))
+            if (int.TryParse(weaponinput, out int parsedInput2))
             {
-                ownedWeapons[parsedInput2-1].Item2.EquipWeapon(player);
+                ownedWeapons[parsedInput2-1].EquipWeapon(player);
                 OpenInventory(player);
             }
 
@@ -79,8 +91,8 @@ public class Inventory
 
         if (int.TryParse(input, out int parsedInput))
         {
-            notEmptyItems[parsedInput-1].Item2.UseItem(player);
-            Console.WriteLine($"{player.Name} used {notEmptyItems[parsedInput-1].Item2}");
+            notEmptyItems[parsedInput-1].UseItem(player);
+            Console.WriteLine($"{player.Name} used {notEmptyItems[parsedInput-1]}");
         }
 
         else 
@@ -88,5 +100,10 @@ public class Inventory
             Console.WriteLine("Invalid Input");
             OpenInventory(player);
         }
+    }
+
+    public void GainItem(string itemId)
+    {
+        
     }
 }
