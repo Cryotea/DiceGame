1cbe2d7ff27a407a61d2d9925cc2067d817bad8c
Cryotea <cryotea@gmail.com>
2023-11-28 14:41:02 +0100

    updatetet GetLoot Methode

--- Loot.cs
+++ Loot.cs
@@ -2,47 +2,19 @@
 using diceGame.Item;
 using diceGame;
 using diceGame.Weapon;
+using diceGame.Enemy;
 namespace diceGame;
 
 
 public class Loot
 {
 
-    public void GetLoot(Player player, IFighter enemy)
+    public void GetLoot(Player player, IEnemy enemy)
     {
-        var random = new Random();
-        int Loot = random.Next(1,3);
-        int RareLootChance = random.Next(1,5);
-        int WeaponDropChance = random.Next(1,4);
-        
-        //use switchcases instead of if
-        if(Loot == 1)
-        {
-            player.Inventory.SmallPotion.Amount ++;
-            Console.WriteLine($"{player.Name} got a {player.Inventory.SmallPotion}");
-        }
-        if(Loot == 2)
-        {
-            player.Inventory.MediumPotion.Amount++;
-            Console.WriteLine($"{player.Name} got a {player.Inventory.MediumPotion}");
-        }
-
-        if (RareLootChance == 1)
-        {
-            int RareLoot = random.Next(1,2);
-
-            if (RareLoot == 1)
-            {
-                player.Inventory.GoldenFonduePizza.Amount++;
-                Console.WriteLine($"{player.Name} got a {player.Inventory.GoldenFonduePizza}");
-            }
-        }
-
-        if (WeaponDropChance == 1)
-        {
-            player.Inventory.GainWeapon(enemy.Weapon.Id);
-            Console.WriteLine($"{player.Name} found a {enemy.Weapon}!");
-        }
+       enemy.LootTable.GetLootTableItems(player);
+       enemy.LootTable.GetWeaponDrop(player, enemy);
+       enemy.GetStats(player);
+       enemy.GetExp(player);
     }
 
 }
