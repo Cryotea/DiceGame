2acb3bfc56731a7c3f2bb667f609dae58f05c5bc
Cryotea <cryotea@gmail.com>
2023-12-07 11:06:48 +0100

    updatet Strength GetStat Methode

--- Stats/Strength.cs
+++ Stats/Strength.cs
@@ -11,6 +11,10 @@ public class Strength : IStat
     public double GetStat(IFighter fighter)
     {
         double Strength = MaxStrength;
+        double Strengthbuff = MaxStrength * (fighter.Effect.Strength.Item1 * 0.1);
+        double Weaknessdebuff = MaxStrength * (fighter.Effect.Weakness.Item1 * 0.1);
+
+        Strength = Strength + Strengthbuff - Weaknessdebuff;
         return Strength;
     }
 }
