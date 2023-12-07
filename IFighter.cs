4c9e9546785eea0c2592115cafbaba806a42b4c3
Cryotea <cryotea@gmail.com>
2023-12-07 11:00:22 +0100

    updatet IFighter to use StatClasses

--- IFighter.cs
+++ IFighter.cs
@@ -1,17 +1,17 @@
 using diceGame.Weapon;
 using diceGame.Effects;
+using diceGame.Stats;
 
 namespace diceGame;
 
 public interface IFighter
 {
     public string Name {get; set;}
-    public double Health {get; set;} 
-    public double Damage {get; set;} 
-    public double Speed {get; set;} 
-    public double Defence {get; set;} 
+    public Health Health {get; set;} 
+    public Stats.Strength Strength {get; set;} 
+    public Speed Speed {get; set;} 
+    public Defence Defence {get; set;} 
     public IWeapon Weapon {get; set;}
-    public double MaxHealth {get; set;}
     public int EXP {get; set;}
     public Effect Effect {get; set;}
     public int Money {get; set;}
