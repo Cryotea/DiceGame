b8e63678586dfb17a5018750f4782e8f79c1320b
Cryotea <cryotea@gmail.com>
2023-12-07 11:08:18 +0100

    added LootTAbleItems

---
+++ LootTableItem.cs
@@ -0,0 +1,20 @@
+using diceGame.Item;
+
+namespace diceGame;
+
+ /*public class LootTableItem
+{
+    public required string ItemId {get; set;}
+    public required int DropChance {get; set;}
+    public int MaxAmount {get; set;}
+
+   public int CalculateLoot()
+    {
+        if(MaxAmount <= 0) MaxAmount = 1;
+        var random = new Random();
+        if (random.Next(1,101) <= DropChance)
+        {
+            
+        }
+    }
+}*/
