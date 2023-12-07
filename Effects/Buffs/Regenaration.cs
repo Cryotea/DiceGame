6bbcf0adbf03cf8490931ed5bae6da3c17b53b9e
Cryotea <cryotea@gmail.com>
2023-12-07 11:03:46 +0100

    updatet Regeneration to use new StatClasses

--- Effects/Buffs/Regenaration.cs
+++ Effects/Buffs/Regenaration.cs
@@ -4,11 +4,11 @@ public class Regenaration : IBuff
 {
     public static void Buff(IFighter user, int Level, int duration)
     {
-        double Healed = user.MaxHealth * ( Level * 0.1);
-        user.Health = user.Health + (user.MaxHealth * ( Level * 0.1));
-        if (user.Health > user.MaxHealth)
+        double Healed = user.Health.MaxHealth * ( Level * 0.1);
+        user.Health.HP = user.Health.HP + (user.Health.MaxHealth * ( Level * 0.1));
+        if (user.Health.HP > user.Health.MaxHealth)
         {
-            user.Health = user.MaxHealth;
+            user.Health.HP = user.Health.MaxHealth;
         }
         Console.WriteLine($"{user.Name}'s Regenaration healed them for {Healed}!\n{user.Name} has {user.Health}Hp now! \nThe duration is of Regenaration is {user.Effect.Regenaration.Item2}");
     }
