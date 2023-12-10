067339d4814bcafa9fa0bcea1d27f00284a5a196
Cryotea <cryotea@gmail.com>
2023-12-11 00:32:26 +0100

    Added string Id to BaseStat.cs

--- Stats/BaseStat.cs
+++ Stats/BaseStat.cs
@@ -3,5 +3,7 @@ namespace diceGame.Stats;
 public class BaseStat
 {
     public double Max {get; set;}
-    public double Current {get; set;} 
+    public double Current {get; set;}
+    
+    public string Id {get; set;}
 }
