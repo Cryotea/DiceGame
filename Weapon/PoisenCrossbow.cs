8be9f0b30c817d28123424a5c3264e4e300cd491
Cryotea <cryotea@gmail.com>
2023-12-12 20:40:47 +0100

    updatet PoisenCrossbow.cs

--- Weapon/PoisenCrossbow.cs
+++ Weapon/PoisenCrossbow.cs
@@ -8,7 +8,7 @@ public class PoisenCrossbow :BaseWeapon, IWeapon
 {
     public string Id {get; set;} = "PoisenCrossbow";
     public int Amount {get; set;} 
-    private int Turn = 1;
+    private int Turn = 0;
     private double Strength = 0;
     public void AttackPattern(IFighter attacker, IFighter defender)
     {
@@ -19,20 +19,22 @@ public class PoisenCrossbow :BaseWeapon, IWeapon
             case 0:
                 Strength = random.Next(0, 11);
                 AnsiConsole.MarkupLine($"\n{attacker.Name} draws the Crossbow with [red]{Strength} Strength[/]");
+                Turn++;
                 break;
             
             case 1:
                 int poisenLevel = random.Next(1,3);
-                int poisenDuration = random.Next(1,3);
+                int poisenDuration = random.Next(1,6);
                 double dice = random.Next(1,6);
 
                 defender.Effect.Poisen.Item1 = poisenLevel;
                 defender.Effect.Poisen.Item2 += poisenDuration;
 
-                double DamageDone = Math.Round((Strength * 0.1) * dice + dice  ,2);
+                double DamageDone = Math.Round((Strength * 0.2) * dice + dice  ,2);
                 defender.Health.Current -= DamageDone;
                 defender.Health.Current = Math.Round(defender.Health.Current, 2);
-                AnsiConsole.MarkupLine($"{attacker.Name} rolled a [red]{dice}[/] and attacked {defender.Name} for [red]{DamageDone} Damage[/]! {defender.Name} has[green] {defender.Health.Current} HP[/] left!");
+                AnsiConsole.MarkupLine($"{attacker.Name} rolled a [red]{dice}[/] and attacked {defender.Name} for [red]{DamageDone} Damage[/]!  {defender.Name} got [springgreen2_1]Poisoned [/]for [springgreen2_1]{poisenDuration}[/] turns! and has[green] {defender.Health.Current} HP[/] left!");
+                Turn++;
                 break;
         }
     }
