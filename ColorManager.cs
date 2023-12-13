6e832265d049ac96dab0809b82a1aa6ee506cdcf
Cryotea <cryotea@gmail.com>
2023-12-13 15:02:50 +0100

    Added ColorManager.cs

---
+++ ColorManager.cs
@@ -0,0 +1,29 @@
+namespace diceGame;
+
+public class ColorManager
+{
+    
+    public static string HealthColor = "[lime]";
+
+    public static string DefenceColor = "[grey39]";
+
+    public static string SpeedColor = "[paleturquoise1]";
+
+    public static string StrengthColor = "[maroon]";
+
+
+    public static string RegenerationColor = "[mediumorchid1_1]";
+
+    public static string PoisonColor = "[springgreen2_1]";
+
+
+    public static string BasicWeaponColor = "[darkorange]";
+
+
+    public static string BasicItemColor = "[lightgoldenrod1]";
+
+    public static string PlayerColor = "[springgreen4]";
+    
+    public static string BasicEnemyColor = "[red]";
+    
+}
