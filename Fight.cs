8eae1a7c1c974c62b58494b105b45b312f392e12
Cryotea <cryotea@gmail.com>
2023-12-21 16:06:22 +0100

    update Fight.cs to allow for boss spawning

--- Fight.cs
+++ Fight.cs
@@ -1,7 +1,7 @@
 namespace diceGame;
 using diceGame.Enemy;
 using diceGame.Weapon;
-
+using diceGame.Enemy.Boss;
 // das hier war der versuch die enemy var zu aendern Edit#2 hab das zu IFighter gemacht funt jedoch trozdem nicht
 public class Fight
 {
@@ -13,6 +13,16 @@ public class Fight
        (2, new EnemySkeletonArcher()) 
 
     };
+    
+    private static readonly (int, IBoss) [] BossEnemies =
+    {
+        (1, new BossSlime()),
+        (1, new BossKnight()),
+        (1, new BossArcher()),
+        (1, new BossSkeletonArcher()) 
+
+    };
+    
     public static IEnemy GetEnemy()
     {
         var random = new Random();
@@ -27,6 +37,17 @@ public class Fight
                 Enemies[attackerNumber].Item1--;
                 return Enemies[attackerNumber].Item2.Clone() as IEnemy;
             }
+
+            if (Enemies[attackerNumber].Item1 == 0)
+            {
+                if (BossEnemies[attackerNumber].Item1 > 0)
+                {
+                    encounters = BossEnemies[attackerNumber].Item1;
+                    BossEnemies[attackerNumber].Item1--;
+                    return BossEnemies[attackerNumber].Item2.Clone() as IEnemy;
+                }
+                
+            }
         }
         return null;
     }
@@ -38,6 +59,12 @@ public class Fight
         {
             AllEnemyEncounters += entry.Item1;
         }
+        
+        foreach(var entry in BossEnemies)
+        {
+            AllEnemyEncounters += entry.Item1;
+        }
+        
     return AllEnemyEncounters;
     }
 }
