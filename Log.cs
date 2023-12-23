16d7025adcf7a6349a788bc65e8f00e0f117d9bf
Cryotea <cryotea@gmail.com>
2023-12-23 20:28:42 +0100

    added DefeatedEnemies array and AddDefeatedEnemy methode

--- Log.cs
+++ Log.cs
@@ -1,3 +1,5 @@
+using diceGame.Enemy;
+using diceGame.Enemy.Boss;
 using Spectre.Console;
 
 namespace diceGame;
@@ -6,11 +8,25 @@ public class Log
 {
     public List<string> MessageLog = new List<string>();
 
+    public (IEnemy, int)[] DefeatedEnemies =
+    {
+        (new EnemySlime(),0),
+        (new EnemyKnight(),0),
+        (new EnemyArcher(),0),
+        (new EnemySkeletonArcher(),0),
+        
+        //Bosses
+        (new BossSlime(),0),
+        (new BossKnight(),0),
+        (new BossArcher(),0),
+        (new BossSkeletonArcher(),0)
+        
+    };
     public void AddMessage(string Message)
     {
         MessageLog.Add(Message);
     }
-
+    //TODO Rename Methode
     public void WriteTwoLatestMessage()
     {
         AnsiConsole.MarkupLine($"{MessageLog[MessageLog.Count - 2]}");
@@ -21,7 +37,16 @@ public class Log
     {
         AnsiConsole.MarkupLine($"{MessageLog[MessageLog.Count - 1]}");
     }
-    
-        
+
+    public void AddDefeatedEnemy(IEnemy enemy)
+    {
+        int index = Array.FindIndex(DefeatedEnemies, item => item.Item1.Id == enemy.Id);
+
+        if (index != -1)
+        {
+            
+            DefeatedEnemies[index].Item2++;
+        }
+    }
     
 }
