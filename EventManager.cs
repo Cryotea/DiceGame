2f9b14cad5504e58c1b24f07fa101313fe9049b6
Cryotea <cryotea@gmail.com>
2024-01-04 20:11:30 +0100

    added Shop event

--- EventManager.cs
+++ EventManager.cs
@@ -5,8 +5,23 @@ namespace diceGame;
 
 public class EventManager
 {
+    private Shop _shop = new Shop();
     public void RandomEvent(Player player, Log log)
     {
+
+        if (log.Fights % 4 == 0 && log.Fights > 0)
+        {
+            var openshop = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("you found a Shop! do you want to enter it?")
+                    .PageSize(10)
+                    .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+                    .AddChoices(new[] {
+                        "yes", "no",
+                    }));
+            if (openshop == "yes")_shop.OpenShop(player, log);
+        }
+        
         if (log.DefeatedEnemies[0].Item2 == 2 && log.DefeatedEnemies[4].Item2 == 1)
         {
             var answer = AnsiConsole.Prompt(
