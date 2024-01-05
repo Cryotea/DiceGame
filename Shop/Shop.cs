79d24897907719dca6ab2b860e713a4fb575bb38
Cryotea <cryotea@gmail.com>
2024-01-05 17:25:06 +0100

    added shop buy and not enough money feature

--- Shop/Shop.cs
+++ Shop/Shop.cs
@@ -23,10 +23,10 @@ public class Shop
 
         var BuyOptions = AnsiConsole.Prompt(
             new SelectionPrompt<IShopItem>()
-                .Title("Welcome to my Shop! What do you want to buy?")
+                .Title($"\nWelcome to my Shop! What do you want to buy? \n {player.Name}'s {ColorManager.MoneyColor} Money : {player.Money}$ [/]")
                 .PageSize(10)
                 .AddChoices(AllShopItems)
-                .UseConverter(item => item.Id == "Exit" ? item.ToString()! : $"{item} for {item.BuyPrice}"));
+                .UseConverter(item => item.Id == "Exit" ? item.ToString()! : $"{item} for {ColorManager.MoneyColor}{item.BuyPrice}$[/]"));
 
         if (BuyOptions.Id == "Exit")
         {
@@ -34,17 +34,30 @@ public class Shop
             return;
         }
 
+        switch (BuyOptions.BuyPrice < player.Money)
+        {
+            case true:
+                var BoughtItem = player.Inventory.AllItems.FirstOrDefault(item => item.Id == BuyOptions.Id);
+                        if (BoughtItem is not null) BoughtItem.Amount++;
+                        
+                        var BoughtWeapons = player.Inventory.AllWeapons.FirstOrDefault(item => item.Id == BuyOptions.Id);
+                        if (BoughtWeapons is not null) BoughtWeapons.Amount++;
+                        
+                        player.Money -= BuyOptions.BuyPrice;
+                
+                        AnsiConsole.Clear();
+                        log.AddMessage($"\n{player.Name} bought {BuyOptions}");
+                        log.WriteTwoLatestMessage();
+                        break;
+            case false:
+                AnsiConsole.Clear();
+                log.AddMessage($"{player.Name} does not have enough {ColorManager.MoneyColor}Money[/] ");
+                log.WriteTwoLatestMessage();
+                break;
+        }
+        
+        OpenShop(player, log);
 
-        var BoughtItem = player.Inventory.AllItems.FirstOrDefault(item => item.Id == BuyOptions.Id);
-        if (BoughtItem is not null) BoughtItem.Amount++;
-        
-        var BoughtWeapons = player.Inventory.AllWeapons.FirstOrDefault(item => item.Id == BuyOptions.Id);
-        if (BoughtWeapons is not null) BoughtWeapons.Amount++;
-        
-        
-        AnsiConsole.Clear();
-        log.AddMessage($"\n{player.Name} bought {BuyOptions}");
-        log.WriteTwoLatestMessage();
     }
     
 }
